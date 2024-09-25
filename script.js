9ee628f1a6402b565b0c0384acaa6b06742f187e
rigolao <mrigolao@gmail.com>
2024-09-24 22:41:20 -0300

    pimbas

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-
-
 const switchButton = document.querySelector('#switch-button');
-
 const model1 = document.querySelector("#magnemite");
 const model2 = document.querySelector("#dragonite");
 
 switchButton.addEventListener('click', () => {
-    console.log(model1.getAttribute("scale"))
     if (model1.getAttribute("scale").x === 0.1) {
-        model1.setAttribute("scale", "0 0 0")
-        model2.setAttribute("scale", "0.01 0.01 0.01")
+        model1.setAttribute("scale", "0 0 0");
+        model2.setAttribute("scale", "0.01 0.01 0.01");
     } else {
-        model1.setAttribute("scale", "0.1 0.1 0.1")
-        model2.setAttribute("scale", "0 0 0")
+        model1.setAttribute("scale", "0.1 0.1 0.1");
+        model2.setAttribute("scale", "0 0 0");
     }
-})
+});
+
+let isMouseDown = false;
+let previousMouseX = 0;
+
+function rotateModel(event) {
+    const magnemiteModel = document.querySelector('#magnemite');
+    const dragoniteModel = document.querySelector('#dragonite');
+    if (isMouseDown) {
+        const deltaX = event.clientX - previousMouseX;
+        const rotation = magnemiteModel.getAttribute('rotation');
+        magnemiteModel.setAttribute('rotation', {
+            x: rotation.x,
+            y: rotation.y + deltaX * 0.5,
+            z: rotation.z
+        });
+        dragoniteModel.setAttribute('rotation', {
+            x: rotation.x,
+            y: rotation.y + deltaX * 0.5,
+            z: rotation.z
+        });
+        previousMouseX = event.clientX;
+    }
+}
+
+document.addEventListener('mousedown', (event) => {
+    isMouseDown = true;
+    previousMouseX = event.clientX;
+});
+
+document.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
+document.addEventListener('mousemove', rotateModel);
